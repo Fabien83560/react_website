fe00a187e80ee5204e3c4079010f64bef17d7d1a
Fabien83560 <fabienortega.290604@gmail.com>
2024-02-07 21:47:38 +0100

    Add Component for each Navigation Item

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,34 @@
 import React from "react"
+import About from "./MainComponents/About.jsx"
+import Skills from "./MainComponents/Skills.jsx"
+import Portfolio from "./MainComponents/Portfolio.jsx"
+import Cv from "./MainComponents/Cv.jsx"
 
 export default function({ selectedItem }) {
+
+    let ComponentToRender;
+
+    switch (selectedItem) {
+        case 'À Propos':
+            ComponentToRender = About;
+            break;
+        case 'Compétences':
+            ComponentToRender = Skills;
+            break;
+        case 'PortFolio':
+            ComponentToRender = Portfolio;
+            break;
+        case 'CV':
+            ComponentToRender = Cv;
+            break;
+        default:
+            ComponentToRender = About;
+    }
 
     return (
         <>
             <main>
-                <p>{selectedItem}</p>
+                <ComponentToRender />
             </main>
         </>
     );
